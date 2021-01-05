c673402d33b78db988c7f5f186099cde06feb59b
Mark DeVito <markdv@alumni.unc.edu>
2021-01-04 19:00:55 -0500

    imports on app.js page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,14 @@
 import './styles/App.css';
+import React, { useState, useEffect} from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import CreateGame from './pages/CreateGame'
+import GamePage from './pages/GamePage'
+import UpdateGame from './pages/UpdateGame'
 
 function App() {
   return (
@@ -7,4 +17,4 @@ function App() {
   )
 }
 
-export default App
+export default withRouter(App)
